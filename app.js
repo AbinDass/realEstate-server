fc646c363807b2f048362acfaebf51a210ac147c
AbinDass <abindas350@gmail.com>
2024-05-10 02:42:45 +0530

    added route and controller folder also created api routes for authentication apis

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 import express from "express";
+import postRoute from './routes/post.route.js'
+import authRoute from './routes/auth.route.js'
 
 const app = express();
-console.log(`testing`);
-
-app.post(`/`, (req, res) => {
-    console.log(req.body);
-    // let a = req.body;
-    let sum = a + b;
-    console.log(sum);
-    res.send(`sum`);
-});
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use('/api/posts', postRoute)
+app.use('/api/auth', authRoute)
 
 app.listen(8000, () => {
     console.log(`server is running at 8000`);
---
+++ controller/auth.controller.js
@@ -0,0 +1,10 @@
+export const register = (req, res) => {
+    //db oprtions 
+    console.log(req.body)
+}
+export const login = (req, res) => {
+    //db oprtions
+}
+export const logout = (req, res) => {
+    //db oprtions
+}
