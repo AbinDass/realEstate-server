2a14490021c445c504ef6b54f179117c793917d7
AbinDass <abindas350@gmail.com>
2024-05-12 00:33:01 +0530

    mongodb database connected success fully using prisma for ORM structure

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,6 +1,21 @@
-export const register = (req, res) => {
+import bcrypt from "bcrypt"
+import prisma from "../lib/prisma.js";
+export const register = async(req, res) => {
     //db oprtions 
-    console.log(req.body)
+    const {username, email , phone , password} = req.body;
+    console.log(username, email , phone , password , `-------`)
+    //hash the Password
+    const hashedPassword = await bcrypt.hash(password, 10)
+    console.log(hashedPassword);
+    const newUser = await prisma.user.create({ 
+        data:{
+            username,
+            email,
+            phone:phone,
+            password:hashedPassword
+        }
+    })
+    console.log(newUser)
 }
 export const login = (req, res) => {
     //db oprtions
